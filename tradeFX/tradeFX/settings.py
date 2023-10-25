d2e2f0d60c4f628bd6753f623ff393c07e17c5ce
dhev-wisdom <nonsowisdom62@gmail.com>
2023-10-25 16:25:07 +0100

    check deploy success

--- tradeFX/tradeFX/settings.py
+++ tradeFX/tradeFX/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
