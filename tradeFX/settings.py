6008202c5b3e0cad843ef0816ef9dee9ec9f1f49
dhev-wisdom <nonsowisdom62@gmail.com>
2023-11-03 11:15:18 +0100

    render deploy

--- tradeFX/settings.py
+++ tradeFX/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost', 'https://tradefx-9a3b9c58de68.herokuapp.com/', '.onrender.com']
 
